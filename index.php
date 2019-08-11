3b6d7185206938d455ad1edfbc9b68393d041aa2
Ully Kharisma Putra <ullykharisma@gmail.com>
2019-08-11 13:58:24 +0700

    change UI index file

--- index.php
+++ index.php
@@ -6,29 +6,48 @@ session_start();
 
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Welcome to Web App</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/style.css ">
-	<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
-</head>
-<body>
-	<div class="header">
-	<a href="/">Exam Partner</a>
-	</div>
+	<head>
+		<title>Welcome to Web App</title>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="assets/css/style.css ">
+		<link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+	</head>
+	<body>
+		<div class="header">
+			<a href="/">Exam Partner</a>
+		</div>
 
-	<?php if( isset($_SESSION['user_id'])): ?>
+		<?php if( isset($_SESSION['user_id'])): ?>
 
-		<br/>Welcome. You are  successfully logged in!
+			<br/>Welcome. You are  successfully logged in!
 
-		<a href="logout.php">Logout? </a>
+			<a href="logout.php">Logout? </a>
 
-	<?php else: ?>
+		<?php else: ?>
 
-		<h1>Login or Register </h1>
-		<a href="login.php">Login </a>or
-		<a href="register.php">Register</a>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-5">
+					<a href="login.php">
+						<div class="content">
+							<p>Login</p>
+						</div>
+					</a>
+				</div>
+				<div class="col-md-2 bold">
+					<h3>OR</h3>
+				</div>
+				<div class="col-md-5">
+					<a href="register.php">
+						<div class="content">
+							<p>Register</p>
+						</div>
+					</a>
+				</div>
+			</div>
+		</div>
 
-	<?php endif; ?> 	
+		<?php endif; ?> 	
 
-</body>
+	</body>
 </html>
